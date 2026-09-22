3d857417528cd724b44ed056fed706aea3b4414b
Iswarya Peram <iswaryaperam11@gmail.com>
2026-09-22 22:50:09 +0530

    Tokens in python

---
+++ Day-02/variables.py
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+Token
+Token is small units of program
+they are 5 types
+ide
+lit
+key
+oper
+pun
+identifiers: is name of the function or variable or classes
+literals : values
+key : predefined functions
+operators: + - * %
+pun: () :
+identifiers
+1 contains uppercase, lowercase,digits,_
+2 no special char
+3 ide can start with uppercase, lowercase,_
+4 case sensitive
+variables:
+=======
+Token
+Token is small units of program
+they are 5 types
+ide
+lit
+key
+oper
+pun
+identifiers: is name of the function or variable or classes
+literals : values
+key : predefined functions
+operators: + - * %
+pun: () :
+identifiers
+1 contains uppercase, lowercase,digits,_
+2 no special char
+3 ide can start with uppercase, lowercase,_
+4 case sensitive
+variables:
+>>>>>>> d288f847551d151c233c234d59ecae175ab21a86
